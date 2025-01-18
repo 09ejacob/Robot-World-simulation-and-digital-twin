3859dd6c037a90ed18ca1088feee4b70c59caf81
09ejacob <elias.garseth.jacobsen@gmail.com>
2025-01-19 00:04:17 +0100

    Added a python file for setting up a scene with ground plane, two cubes, and surface gripper action graph attached to cube1. The surface gripper can be toggled on and off, simulating the suction cup gripper on the grab robot. Also slightly modified the README file.

---
+++ Ny mappe/SetupSceneAndSurfaceGripperTest.py
@@ -0,0 +1,80 @@
+import numpy as np
+from omni.isaac.core.objects import DynamicCuboid
+from omni.isaac.core.objects.ground_plane import GroundPlane
+from omni.isaac.core.simulation_context import SimulationContext
+from omni.isaac.surface_gripper import SurfaceGripper
+from omni.isaac.core.physics_context import PhysicsContext
+import omni.graph.core as og
+from pxr import UsdGeom, Gf
+import omni.usd
+
+def create_groundPlane(path):
+    PhysicsContext()
+    GroundPlane(prim_path=path, size=10, color=np.array([0.5, 0.5, 0.5]))
+    print("Created ground plane")
+
+def setup_start_cubes(prim_path1, prim_path2, position1=(0, 0, 0), scale1=(1, 1, 1), color1=(0, 0, 0),
+                        position2=(0, 0, 0), scale2=(1, 1, 1), color2=(0, 0, 0)):
+    cube1 = DynamicCuboid(
+        prim_path=prim_path1,
+        position=np.array(position1),
+        scale=np.array(scale1),
+        color=np.array(color1)
+    )
+    print("Created cube1")
+
+    cube2 = DynamicCuboid(
+        prim_path=prim_path2,
+        position=np.array(position2),
+        scale=np.array(scale2),
+        color=np.array(color2)
+    )
+    print("Created cube2")
+
+def create_surfaceGripper_actionGraph(graph_path, grip_position_path, parent_rigidBody_path):
+    keys = og.Controller.Keys
+    (graph_handle, list_of_nodes, _, _) = og.Controller.edit(
+        {"graph_path": graph_path, "evaluator_name": "execution"},
+        {
+            keys.CREATE_NODES: [
+                ("surface_gripper", "omni.isaac.surface_gripper.SurfaceGripper"),
+                ("close", "omni.graph.action.OnImpulseEvent"),
+                ("impulse_monitor", "omni.graph.action.OnImpulseEvent"),
+                ("open", "omni.graph.action.OnImpulseEvent"),
+            ],
+            keys.SET_VALUES: [
+                ("surface_gripper.inputs:GripPosition", grip_position_path),
+                ("surface_gripper.inputs:ParentRigidBody", parent_rigidBody_path),
+            ],
+            keys.CONNECT: [
+                ("impulse_monitor.outputs:execOut", "surface_gripper.inputs:onStep"),
+                ("open.outputs:execOut", "surface_gripper.inputs:Open"), # open
+                ("close.outputs:execOut", "surface_gripper.inputs:Close"), # close
+            ],
+        },
+    )
+    print("Created surface gripper action graph")
+
+def create_xform(path, translate=(0, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1)):
+    stage = omni.usd.get_context().get_stage()
+    xform = UsdGeom.Xform.Define(stage, path)
+
+    xform.AddTranslateOp().Set(Gf.Vec3d(*translate))
+    xform.AddRotateXYZOp().Set(Gf.Vec3f(*rotation))
+    xform.AddScaleOp().Set(Gf.Vec3f(*scale))
+    
+    print("Created Xform")
+
+def setup_scene():
+    print("Setting up scene...")
+
+    # Objects
+    create_groundPlane("/World/groundPlane")
+    setup_start_cubes("/World/cube1", "/World/cube2", position1=(0.0, 0.0, 2), scale1=(1, 1, 1), color1=(0.2, 0.5, 0.7),
+                        position2=(0.0, 0.0, 0.5), scale2=(1, 1, 1), color2=(0.7, 0.5, 0.2))
+
+    # Surface gripper related
+    create_surfaceGripper_actionGraph("/World/cube1/SurfaceGripperActionGraph", "/World/cube1/SurfaceGripperActionGraph/SurfaceGripperOffset", "/World/cube1")
+    create_xform("/World/cube1/SurfaceGripperActionGraph/SurfaceGripperOffset", translate=(0, 0, -0.500997), rotation=(0, 0, 0), scale=(1, 1, 1))
+
+setup_scene()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Robot-World-simulation-and-digital-twin---Bachelor-group-9-2025
 Bachelor task for group 9 - Computer engineering - Simulate robot in Isaac Sim
+
+HUSK Å GJØRE REPOET PUBLIC FØR LEVERING!
