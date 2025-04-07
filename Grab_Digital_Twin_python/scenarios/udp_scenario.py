e61be5b49e37c0ee5a4d5af16d0f312334c6908f
Elias Garseth Jacobsen <elias.garseth.jacobsen@gmail.com>
2025-04-07 15:03:32 +0200

    Added parsing and feature for starting and stopping a recording with the new camera.

--- Grab_Digital_Twin_python/scenarios/udp_scenario.py
+++ Grab_Digital_Twin_python/scenarios/udp_scenario.py
@@ -17,6 +17,7 @@ from ..global_variables import (
     AXIS4_JOINT_PATH,
     ENVIRONMENT_PATH,
     SHELF_PATH,
+    OVERVIEW_CAMERA,
 )
 from ..networking.udp_controller import UDPController
 from omni.isaac.core.objects import DynamicCuboid
@@ -59,6 +60,10 @@ class UDPScenario:
         self.broadcast_target_port = self.SEND_PORT
         self.last_position_print_time = time.time()
 
+        self.overview_camera_active = False
+        self.last_overview_capture_time = 0
+        self.overview_capture_interval = 0.1
+
         self.axis_config = [
             ("axis1", AXIS1_JOINT_PATH, True),
             ("axis2", AXIS2_JOINT_PATH, False),
@@ -66,8 +71,6 @@ class UDPScenario:
             ("axis4", AXIS4_JOINT_PATH, True),
         ]
         self.axis_dofs = []
-
-
 
     def _udp_callback(self, message):
         """Receives UDP messages and stores them in a queue."""
@@ -109,6 +112,15 @@ class UDPScenario:
             return
 
         command = parts[0].lower()
+
+        if command == "start_overview_camera":
+            self.overview_camera_active = True
+            print("Overview-camera capturing started.")
+            return
+        elif command == "stop_overview_camera":
+            self.overview_camera_active = False
+            print("Overview-camera capturing stopped.")
+            return
 
         handlers = {
             "tp_robot": self._handle_tp_robot,
@@ -411,6 +423,13 @@ class UDPScenario:
             self.print_box_position("/World/Environment/stack2/box_2_19")
             
             self.last_position_print_time = start_time
+
+        if self.overview_camera_active:
+            if start_time - self.last_overview_capture_time >= self.overview_capture_interval:
+                result = self._robot_controller.camera_capture.capture_image("OverviewCamera")
+                if result:
+                    print("Overview camera captured image:", result)
+                self.last_overview_capture_time = start_time
 
     def print_box_position(self, box_path):
         stage = omni.usd.get_context().get_stage()
